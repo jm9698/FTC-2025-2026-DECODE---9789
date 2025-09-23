e2bf724344d1941772fa69091efb4e57e310e5c9
MenaceMaster9600 <jm9876000@gmail.com>
2025-09-23 15:43:23 -0400

    WIP Version

--- Current Season/Autos.js
+++ Current Season/Autos.js
@@ -8,18 +8,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "Auto (Updated-Testing) (Blocks to Java)")
+@Autonomous(name = "Auto (First 2025-26 Version)")
 public class Autos extends LinearOpMode {
 
 private DcMotor backleft;
 private DcMotor backright;
 private DcMotor frontleft;
 private DcMotor frontright;
-private DcMotor leftarm;
-private DcMotor rightarm;
-private DcMotor linearslide;
-private Servo gclaw;
-private Servo rclaw;
+// Add attachments as required
 
 private ElapsedTime runtime = new ElapsedTime();
 
@@ -31,11 +27,7 @@ private ElapsedTime runtime = new ElapsedTime();
     backright = hardwareMap.get(DcMotor.class, "backright");
     frontleft = hardwareMap.get(DcMotor.class, "frontleft");
     frontright = hardwareMap.get(DcMotor.class, "frontright");
-    leftarm = hardwareMap.get(DcMotor.class, "leftarm");
-    rightarm = hardwareMap.get(DcMotor.class, "rightarm");
-    linearslide = hardwareMap.get(DcMotor.class, "linearslide");
-    gclaw = hardwareMap.get(Servo.class, "gclaw");
-    rclaw = hardwareMap.get(Servo.class, "rclaw");
+    //Add attachments to hardwareMap as needed
     runtime.reset();
 waitForStart();
     if (opModeIsActive()) {
@@ -44,157 +36,16 @@ waitForStart();
       backright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
       frontleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
       frontright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-      leftarm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-      rightarm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-      linearslide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-      //Swap gclaw zeros to ones
-      gclaw.setPosition(0);
-      rclaw.setPosition(0.5);
+      //Set other motors' modes as required
+      
+      //Set inital servo positions as required
 
     while (opModeIsActive()) {
     if (runtime.seconds() <= 2){
-    backleft.setPower(-0.3);
-    backright.setPower(-0.3);
-    frontleft.setPower(-0.3);
-    frontright.setPower(-0.3);
-    }
-    else if (runtime.seconds() <= 3){
-    backleft.setPower(0);
-    backright.setPower(0);
-    frontleft.setPower(0);
-    frontright.setPower(0);
-    }
-    else if (runtime.seconds() <= 4){
-      linearslide.setPower(-0.6);
-    }
-    else if (runtime.seconds() <= 5){
-      rightarm.setPower(-0.2);
-      leftarm.setPower(0.2);
-    }
-    else if (runtime.seconds() <= 6){
-      linearslide.setPower(0);
-      rightarm.setPower(0);
-      leftarm.setPower(0);
-    }
-    else if (runtime.seconds() <= 7){
-      gclaw.setPosition(0.6);
-    }
-    else if (runtime.seconds() <= 8){
-      rightarm.setPower(-0.2);
-      leftarm.setPower(0.2);
-      linearslide.setPower(0.6);
-    }
-    else if (runtime.seconds() <= 9){
-      linearslide.setPower(0);
-      rightarm.setPower(0);
-      leftarm.setPower(0);
-    }
-    else if (runtime.seconds() <= 10.5){
     backleft.setPower(0.3);
     backright.setPower(0.3);
-    frontleft.setPower(0.3);
-    frontright.setPower(0.3);
-    }
-    else if (runtime.seconds() <= 11.65){
-    backleft.setPower(-0.5);
-    backright.setPower(0.5);
-    frontleft.setPower(-0.5);
-    frontright.setPower(0.5);
-    }
-    else if (runtime.seconds() <= 14.5){
-    backleft.setPower(-0.3);
-    backright.setPower(-0.3);
     frontleft.setPower(-0.3);
     frontright.setPower(-0.3);
-    }
-    else if (runtime.seconds() <= 14.65){
-    backleft.setPower(-0.5);
-    backright.setPower(0.5);
-    frontleft.setPower(-0.5);
-    frontright.setPower(0.5);
-    }
-    else if (runtime.seconds() <= 15.5){
-    backleft.setPower(0);
-    backright.setPower(0);
-    frontleft.setPower(0);
-    frontright.setPower(0);
-    }
-    else if (runtime.seconds() <= 16.5){
-      rightarm.setPower(-0.4);
-      leftarm.setPower(0.4);
-      }
-    else if (runtime.seconds() <= 17.5){
-      linearslide.setPower(-0.6);
-      rightarm.setPower(0);
-      leftarm.setPower(0);
-     }
-     else if (runtime.seconds() <= 18.5){
-      linearslide.setPower(0);
-      rightarm.setPower(0.15);
-      leftarm.setPower(-0.15);
-     }
-     else if (runtime.seconds() <= 19){
-      gclaw.setPosition(0.6);
-     }
-     else if (runtime.seconds() <= 20){
-      rightarm.setPower(-0.2);
-      leftarm.setPower(0.2);
-     }
-     else if (runtime.seconds() <= 21){
-      rightarm.setPower(0);
-      leftarm.setPower(0);
-      linearslide.setPower(0.6);
-     }
-     else if (runtime.seconds() <= 22){
-      rightarm.setPower(0.15);
-      leftarm.setPower(-0.15);
-      linearslide.setPower(0);
-     }
-     else if (runtime.seconds() <= 22.5){
-    rightarm.setPower(0);
-    leftarm.setPower(0);
-    backleft.setPower(0.5);
-    backright.setPower(-0.5);
-    frontleft.setPower(0.5);
-    frontright.setPower(-0.5);
-     }
-     else if (runtime.seconds() <= 25.5){
-    backleft.setPower(0.3);
-    backright.setPower(0.3);
-    frontleft.setPower(0.3);
-    frontright.setPower(0.3);
-     }
-     else if (runtime.seconds() <= 26.5){
-    backleft.setPower(0.5);
-    backright.setPower(-0.5);
-    frontleft.setPower(0.5);
-    frontright.setPower(-0.5);
-     }
-     else if (runtime.seconds() <= 27.75){
-    backleft.setPower(-0.3);
-    backright.setPower(-0.3);
-    frontleft.setPower(-0.3);
-    frontright.setPower(-0.3);
-     }
-     else if (runtime.seconds() <= 28.5){
-    backleft.setPower(-0.5);
-    backright.setPower(0.5);
-    frontleft.setPower(-0.5);
-    frontright.setPower(0.5);
-     }
-     else if (runtime.seconds() <= 29){
-    rightarm.setPower(-0.4);
-    leftarm.setPower(0.4);
-    linearslide.setPower(-0.3);
-    }
-    else if (runtime.seconds() <= 29.5){
-    rightarm.setPower(0.15);
-    leftarm.setPower(-0.15);
-    linearslide.setPower(0);
-    }
-    else if (runtime.seconds() <= 30){
-    rightarm.setPower(0);
-    leftarm.setPower(0);
     }
         telemetry.addData("Frontleft Pow", frontleft.getPower());
         telemetry.addData("Front Right Pow", frontright.getPower());
@@ -202,9 +53,7 @@ waitForStart();
         telemetry.addData("Back Right Pow", backright.getPower());
         telemetry.addData("Right Arm Pow", rightarm.getPower());
         telemetry.addData("Left Arm Pow", leftarm.getPower());
-        telemetry.addData("Linearslide Pow", linearslide.getPower());
-        telemetry.addData("gClaw Pos", gclaw.getPosition());
-        telemetry.addData("rClaw Pos", rclaw.getPosition());
+        //Add any extra telemetry
         telemetry.addData("Runtime", runtime.toString());
         telemetry.update();
 }
