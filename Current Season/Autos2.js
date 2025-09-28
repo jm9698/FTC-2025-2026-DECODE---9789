44bc8ed791566ceec702c859bd772a58b19dba11
MenaceMaster9600 <jm9876000@gmail.com>
2025-09-28 15:20:41 -0400

    Sync With Onbot Version

---
+++ Current Season/Autos2.js
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Auto - Taxi")
+public class Autos2 extends LinearOpMode {
+
+private DcMotor backleft;
+private DcMotor backright;
+private DcMotor frontleft;
+private DcMotor frontright;
+// Add attachments as required
+
+private ElapsedTime runtime = new ElapsedTime();
+
+
+
+@Override
+  public void runOpMode() {
+    backleft = hardwareMap.get(DcMotor.class, "backleft");
+    backright = hardwareMap.get(DcMotor.class, "backright");
+    frontleft = hardwareMap.get(DcMotor.class, "frontleft");
+    frontright = hardwareMap.get(DcMotor.class, "frontright");
+    //Add attachments to hardwareMap as needed
+    runtime.reset();
+waitForStart();
+    if (opModeIsActive()) {
+      runtime.reset();
+      backleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      backright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      frontleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      frontright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+      //Set other motors' modes as required
+    }
+      //Set inital servo positions as required
+
+    while (opModeIsActive()) {
+    if (runtime.seconds() <= 2){
+    backleft.setPower(-0.3);
+    backright.setPower(-0.3);
+    frontleft.setPower(-0.3);
+    frontright.setPower(-0.3);
+    }
+    //Taxi for any amount of time
+    else if (runtime.seconds() <= 5) {
+    backleft.setPower(0);
+    backright.setPower(0);
+    frontleft.setPower(0);
+    frontright.setPower(0);
+    }
+    //Stop at certain interval
+    
+        telemetry.addData("Frontleft Pow", frontleft.getPower());
+        telemetry.addData("Front Right Pow", frontright.getPower());
+        telemetry.addData("Back Left Pow", backleft.getPower());
+        telemetry.addData("Back Right Pow", backright.getPower());
+        //Add any extra telemetry
+        telemetry.addData("Runtime", runtime.toString());
+        telemetry.update();
+    };
+  };
+};
