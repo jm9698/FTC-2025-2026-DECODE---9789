9c3cd8bb7cea5001b85ea95ec2fae3d92cd599d4
MenaceMaster9600 <jm9876000@gmail.com>
2025-10-07 16:19:16 -0400

    Sync With Onbot Version

--- Current Season/Auto-Red-Middle.js
+++ Current Season/Auto-Red-Middle.js
@@ -79,7 +79,7 @@ waitForStart();
     frontright.setPower(0.3);
     }
     //Stop 1 s
-        telemetry.addData("Frontleft Pow", frontleft.getPower());
+        telemetry.addData("Front left Pow", frontleft.getPower());
         telemetry.addData("Front Right Pow", frontright.getPower());
         telemetry.addData("Back Left Pow", backleft.getPower());
         telemetry.addData("Back Right Pow", backright.getPower());
